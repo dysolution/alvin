871ecc6b3bf464c893b5649c0b94fdcc8ab227ab
Jordan Peterson <dysolution@gmail.com>
2012-07-06 21:22:45 -0500

    install Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alvin::Application.config.secret_token = 'e08ccab3175732e038eddc8427f592e7fc7a6e38a20bb81f39080e9db465988a10d1bc833b6b318c64f4a62950bcfbddab4b2088e21624b281f2d416202a6485'
